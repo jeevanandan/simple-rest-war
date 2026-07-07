6f3acf592fb794dab96b34e686f6db9bb6458850
Jeevanandan Ravindran <jeevanandankg@gmail.com>
2026-07-07 19:08:03 +0530

    Remove comment block in RootResource.java
    Removed unnecessary comment block for the RootResource class.

--- src/main/java/org/apache/slider/war/RootResource.java
+++ src/main/java/org/apache/slider/war/RootResource.java
@@ -4,9 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-/**
- * Root resource
- */
 @Path("/")
 public class RootResource {
 
